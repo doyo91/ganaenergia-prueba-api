c3e9a40eb6df7c9b617574316dfe0550e339d850
doyo91 <samuel.gil91dev@gmail.com>
2021-05-11 21:43:36 +0200

    Add Heroku

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get("/", (req, res) => {
   })
 })
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT
 const server = app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
